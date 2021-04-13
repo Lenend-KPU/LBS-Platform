e874ae57ebc91a5a84d195622362e99391b23880
박찬희 <chanhee@PCH-MacBookAir.local>
2021-04-13 14:27:03 +0900

    [feat/back] place모델 rate필드 추가 - #55

--- backend/placeapp/admin.py
+++ backend/placeapp/admin.py
@@ -2,6 +2,6 @@ from placeapp.models import Place
 from django.contrib import admin
 
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ['id', 'profile', 'name', 'photo', 'date', 'private']
+    list_display = ['id', 'profile', 'name', 'rate', 'photo', 'date', 'private']
 
 admin.site.register(Place, PlaceAdmin)
--- backend/placeapp/models.py
+++ backend/placeapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Place(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='place')
     name = models.CharField(max_length=20)
+    rate = models.CharField(max_length=20)
     photo = models.CharField(max_length=20)
     date = models.DateTimeField(auto_now_add=True)
     private = models.BooleanField(default=False)

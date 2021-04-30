6ce4009f5f90bd470f27372da08c17f69f4b11e0
jaeseo lee <dlwotj0914@naver.com>
2021-05-01 06:44:19 +0900

    [fix/back] TODO 추가
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- backend/commentapp/views.py
+++ backend/commentapp/views.py
@@ -8,6 +8,7 @@ from .models import Comment
 # Create your views here.
 class RootView(View):
     def get(self, request) -> HttpResponse:
+        # 리미트 TODO
         comments = Comment.objects.all()
         comments = utils.to_dict(comments)
         result = responses.ok
@@ -21,9 +22,14 @@ class RootView(View):
 
 class ElementView(View):
     def get(self, request, profile_id: int) -> HttpResponse:
+        # 다른 외래키 TODO
+        # TODO
         print(profile_id)
         return utils.send_json(responses.ok)
 
     def post(self, request, profile_id: int) -> HttpResponse:
+        # 다른 파라미터 TODO
+        # profile_id 매개변수 삭제?
+        # TODO
         print(profile_id)
         return utils.send_json(responses.ok)

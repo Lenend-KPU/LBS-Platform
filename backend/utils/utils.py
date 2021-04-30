36d50fd4aa43864105aa8a141b4a70a161641d85
jaeseo lee <dlwotj0914@naver.com>
2021-05-01 03:58:29 +0900

    [refactor/back] JWT 관련 레거시 코드 삭제
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -23,29 +23,6 @@ def pop_args(dic, *args):
     return {arg: dic[arg] if arg in dic else None for arg in args}
 
 
-# def encode_jwt(dic):
-#     return jwt.encode(dic, SECRET_KEY, JWT_ALGORITHM)
-
-
-# # 세션에서 디코딩된 JWT 딕셔너리를 가져오는 헬퍼 함수
-# def decode_jwt(session):
-#     return jwt.decode(session["JWT_TOKEN"], SECRET_KEY, JWT_ALGORITHM)
-
-
-# def append_jwt_param(session, **kargs):
-#     decoded = decode_jwt(session)
-#     for (k, v) in kargs:
-#         decoded[k] = v
-#     return encode_jwt(decoded)
-
-
-# # 원본 session을 변경시키는 함수
-# def delete_jwt_param(session, *args):
-#     decoded = decode_jwt(session)
-#     session["JWT_TOKEN"] = encode_jwt(dict(filter(lambda x: x not in args, decoded)))
-#     return True
-
-
 # request.body로 받아왔을때 binary를 dict로 변환하는 함수
 def byte_to_dict(data):
     body_unicode = data.decode("utf-8")

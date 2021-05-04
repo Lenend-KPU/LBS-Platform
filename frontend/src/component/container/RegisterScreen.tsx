9a75155dbaf2a8705edbf2ea7277bf8c3d98a89c
jaeseo lee <dlwotj0914@naver.com>
2021-05-04 21:04:48 +0900

    [feat/front] RN 레거시 코드 삭제
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/src/component/container/RegisterScreen.tsx
+++
@@ -1,8 +0,0 @@
-import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
-import * as React from "react";
-
-export default function RegisterScreen(props: {
-  navigation: StackNavigationHelpers;
-}): React.ReactElement {
-  return <></>;
-}

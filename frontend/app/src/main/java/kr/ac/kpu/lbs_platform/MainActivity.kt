b4715a573e4b008682ff13021de93aab979ad5b6
jaeseo lee <dlwotj0914@naver.com>
2021-05-05 14:03:45 +0900

    [feat/front] 로그인 액티비티 생성
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/MainActivity.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/MainActivity.kt
@@ -3,9 +3,9 @@ package kr.ac.kpu.lbs_platform
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }

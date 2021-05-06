e6c0fe1b909381759020c53fb1afb27d49a09a59
jaeseo lee <dlwotj0914@naver.com>
2021-05-06 14:11:37 +0900

    [feat/front] ProfileActivity 상단 탭 구현
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/ProfileActivity.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/ProfileActivity.kt
@@ -1,15 +1,25 @@
 package kr.ac.kpu.lbs_platform
 
 import android.graphics.Color
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kpu.lbs_platform.databinding.ActivityMainBinding
+import kr.ac.kpu.lbs_platform.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         window.statusBarColor = getColor(R.color.status_bar_color)
+
+        binding.profileActivityImageView.background = ShapeDrawable(OvalShape())
+        binding.profileActivityImageView.clipToOutline = true
 
     }
 }

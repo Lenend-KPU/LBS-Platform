28e4a6dd8091bf391fcc4b47047acccd6e937270
jaeseo lee <dlwotj0914@naver.com>
2021-05-07 21:38:06 +0900

    [feat/front] AddDocumentFragment, AddPlaceFragment 추가
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/AddPlaceFragment.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/AddPlaceFragment.kt
@@ -1,18 +1,36 @@
 package kr.ac.kpu.lbs_platform.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import kr.ac.kpu.lbs_platform.R
+import kr.ac.kpu.lbs_platform.activity.MainActivity
+
 
 class AddPlaceFragment : Fragment() {
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_place, container, false)
+        val inflated = inflater.inflate(R.layout.fragment_add_place, container, false)
+
+        val placeSearchEditText = inflated.findViewById<EditText>(R.id.placeSearchEditText)
+        placeSearchEditText.setOnClickListener {
+            val fields = listOf(Place.Field.ID, Place.Field.NAME)
+            MainActivity.instance?.let {
+                val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+                    .build(it)
+                startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+            }
+        }
+        return inflated
     }
 }

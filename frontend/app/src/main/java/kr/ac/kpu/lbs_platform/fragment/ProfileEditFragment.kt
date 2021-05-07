95c773108b2660d0e06dd097121456a805583c68
jaeseo lee <dlwotj0914@naver.com>
2021-05-07 22:07:38 +0900

    [refactor/front] 자동 생성된 fragment 코드 수정
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/ProfileEditFragment.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/ProfileEditFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import kr.ac.kpu.lbs_platform.R
 
-class FollowingFragment : Fragment() {
+class ProfileEditFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +17,7 @@ class FollowingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_following, container, false)
+        return inflater.inflate(R.layout.fragment_profile_edit, container, false)
     }
+
 }

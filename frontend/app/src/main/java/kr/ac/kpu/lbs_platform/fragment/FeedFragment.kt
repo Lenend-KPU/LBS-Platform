abccefc8a928039d71b40179fea161b6a7adff2d
jaeseo lee <dlwotj0914@naver.com>
2021-05-07 22:12:16 +0900

    [feat/front] topNavBar 로직 추가
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/FeedFragment.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/FeedFragment.kt
@@ -2,6 +2,7 @@ package kr.ac.kpu.lbs_platform.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -20,5 +21,31 @@ class FeedFragment : Fragment() {
         MainActivity.instance?.binding?.bottomNavigationView?.selectedItemId = R.id.page_feed
         // Inflate the layout for this fragment
         return inflated
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.generate_place -> {
+                MainActivity.instance?.let {
+                    it.supportFragmentManager.beginTransaction().replace(R.id.mainActivityfragment, AddPlaceFragment()).commit()
+                }
+            }
+            R.id.generate_route -> {
+                MainActivity.instance?.let {
+                    it.supportFragmentManager.beginTransaction().replace(R.id.mainActivityfragment, AddDocumentFragment()).commit()
+                }
+            }
+            R.id.saved -> {
+                MainActivity.instance?.let {
+                    it.supportFragmentManager.beginTransaction().replace(R.id.mainActivityfragment, SavedLocationFragment()).commit()
+                }
+            }
+            R.id.profile_edit -> {
+                MainActivity.instance?.let {
+                    it.supportFragmentManager.beginTransaction().replace(R.id.mainActivityfragment, ProfileEditFragment()).commit()
+                }
+            }
+        }
+        return true
     }
 }

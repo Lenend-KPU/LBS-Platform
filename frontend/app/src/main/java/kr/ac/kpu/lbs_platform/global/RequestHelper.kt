3d3643d516c997a6fe03535b060667d4687820b7
jaeseo lee <dlwotj0914@naver.com>
2021-05-08 20:13:32 +0900

    [feat/front] PlaceFragment 구현
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/global/RequestHelper.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/global/RequestHelper.kt
@@ -8,6 +8,8 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kr.ac.kpu.lbs_platform.R
 import splitties.toast.toast
 import java.nio.charset.Charset
@@ -16,7 +18,7 @@ object RequestHelper {
     private val defaultBodyContentType = "application/x-www-form-urlencoded; charset=UTF-8"
     fun request (
         currentFragment: Fragment,
-        destFragment: Fragment,
+        destFragment: Fragment? = null,
         urlParameter: String = "",
         params: Map<String, String> = mapOf(),
         method: Int = Request.Method.POST,
@@ -24,47 +26,60 @@ object RequestHelper {
         fragmentName: String = poko.name,
         queue: RequestQueue = Volley.newRequestQueue(currentFragment.activity),
         bodyContentType: String = defaultBodyContentType,
+        isAsync: Boolean = true,
         onFailureCallback: (responseObject: kr.ac.kpu.lbs_platform.poko.remote.Request) -> Unit = {
             toast(it.comment)
         },
         onSuccessCallback: (responseObject: kr.ac.kpu.lbs_platform.poko.remote.Request) -> Unit = {
-            FragmentChanger.change(currentFragment, destFragment)
+            destFragment?.let {
+                FragmentChanger.change(currentFragment, it)
+            }
         }
     ) {
-        val req: StringRequest = object : StringRequest(
-            method, "${ServerUrl.url}/$urlParameter",
-            { response ->
-                Log.i("LoginFragment", response.toString())
-                val gson = Gson()
-                val responseObject = gson.fromJson(response, poko)
-                if(!responseObject.success) {
-                    currentFragment.toast(responseObject.toString())
-                } else {
-                    onSuccessCallback(responseObject)
-                }
-            }, { error ->
-                Log.i(fragmentName, error.toString())
-                error?.networkResponse?.data?.let {
-                    val responseBody = String(it, Charset.defaultCharset())
+        val fn = {
+            val req: StringRequest = object : StringRequest(
+                method, "${ServerUrl.url}/$urlParameter",
+                { response ->
+                    Log.i("LoginFragment", response.toString())
                     val gson = Gson()
-                    val request = gson.fromJson(responseBody, poko)
-                    Log.i(fragmentName, request.toString())
-                    onFailureCallback(request)
+                    val responseObject = gson.fromJson(response, poko)
+                    if(!responseObject.success) {
+                        currentFragment.toast(responseObject.toString())
+                    } else {
+                        onSuccessCallback(responseObject)
+                    }
+                }, { error ->
+                    Log.i(fragmentName, error.toString())
+                    error?.networkResponse?.data?.let {
+                        val responseBody = String(it, Charset.defaultCharset())
+                        val gson = Gson()
+                        val request = gson.fromJson(responseBody, poko)
+                        Log.i(fragmentName, request.toString())
+                        onFailureCallback(request)
+                    }
                 }
-            }
-        ) {
-            override fun getBodyContentType(): String {
-                if(bodyContentType != defaultBodyContentType || method == Request.Method.POST) {
-                    return bodyContentType
+            ) {
+                override fun getBodyContentType(): String {
+                    if(bodyContentType != defaultBodyContentType || method == Request.Method.POST) {
+                        return bodyContentType
+                    }
+                    return ""
                 }
-                return ""
-            }
 
-            @Throws(AuthFailureError::class)
-            override fun getParams(): Map<String, String> {
-                return params
+                @Throws(AuthFailureError::class)
+                override fun getParams(): Map<String, String> {
+                    return params
+                }
             }
+            queue.add(req)
         }
-        queue.add(req)
+
+        if(isAsync) {
+            GlobalScope.launch {
+                fn()
+            }
+        } else {
+            fn()
+        }
     }
 }

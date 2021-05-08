219c974bcce28f8b20dac49a16a49bf0bbea5259
jaeseo lee <dlwotj0914@naver.com>
2021-05-08 20:23:50 +0900

    [feat/front] ProfileFragment 개선
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/ProfileFragment.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/fragment/ProfileFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kr.ac.kpu.lbs_platform.R
 import kr.ac.kpu.lbs_platform.global.FragmentChanger
+import kr.ac.kpu.lbs_platform.global.Profile
 
 class ProfileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +18,27 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         val inflated = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val profileTextView = inflated.findViewById<TextView>(R.id.profileTextView)
+
+        profileTextView.text = Profile.profile?.let {
+            return@let it.fields.profile_name
+        } ?: ""
+
+        val follwingCountTextView = inflated.findViewById<TextView>(R.id.followingCountTextView)
+        val followersCountTextView = inflated.findViewById<TextView>(R.id.followersCountTextView)
+
+        follwingCountTextView.text = Profile.profile?.let {
+            return@let it.fields.profile_following.toString()
+        } ?: "0"
+        followersCountTextView.text = Profile.profile?.let {
+            return@let it.fields.profile_follower.toString()
+        } ?: "0"
+
         val routesRadioButton = inflated.findViewById<RadioButton>(R.id.routesRadioButton)
         val placesRadioButton = inflated.findViewById<RadioButton>(R.id.placesRadioButton)
-
 
         routesRadioButton.setOnClickListener {
             routesRadioButton.isChecked = true

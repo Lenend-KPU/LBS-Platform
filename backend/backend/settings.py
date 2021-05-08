a6518384135a065ca34d3610c4dca97db827d7c0
Wooogy <hswook12@me.com>
2021-05-08 14:21:58 +0900

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret_key.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # DEFAULT_AUTO_KEY
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"

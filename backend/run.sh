f99bea521e6ecb16b975fed5ebc316ae2067130f
jiho lee <jhlee@autocrypt.io>
2021-05-21 02:08:54 +0900

    환경변수 주입

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+export Django_secret_key='_44xeg*1w4*jn^l=c$9p=hh*b#=2vz1z%+_wuk6ch*v!tz!=$*'
+export DJANGO_SUPERUSER_PASSWORD='123qwe'
+export DJANGO_SUPERUSER_USERNAME='wooogy'
+export DJANGO_SUPERUSER_EMAIL='hswook5347@gmail.com'
+export POSTGRES_USER='django'
+export POSTGRES_PASSWORD='postgres'
+export POSTGRES_DB='lbs_platform'
 export CURDIR=$(pwd);cat deployment.yml | envsubst | kubectl apply -f -

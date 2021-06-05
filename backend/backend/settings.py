9ecf7ec00f2c9019ea49273cebbd9effc70f9c1f
SeungWookHan <hswook12@me.com>
2021-06-05 19:11:35 +0900

    [feat/back] elasticsearch docker 네트워크 external로 묶기 - #123
    Co-authored-by: DPS0340 <optional.int@kakao.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 ALLOWED_HOSTS = ["*"]
 
-elastic_search_url = "http://127.0.0.1:9200/feed"
+elastic_search_url = "http://es01:9200/feed"
 
 
 # Application definition

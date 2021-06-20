2bba858f021417281593689bb67ca009a9e94ceb
jiho lee <optional.int@kakao.com>
2021-06-21 02:24:05 +0900

    [fix/front] BackButton 눌렀을때 이전 fragment로 이동하게끔 수정

--- frontend/app/src/main/java/kr/ac/kpu/lbs_platform/global/FragmentChanger.kt
+++ frontend/app/src/main/java/kr/ac/kpu/lbs_platform/global/FragmentChanger.kt
@@ -3,12 +3,14 @@ package kr.ac.kpu.lbs_platform.global
 import androidx.fragment.app.Fragment
 import kr.ac.kpu.lbs_platform.R
 import kr.ac.kpu.lbs_platform.fragment.ProfileFragment
+import splitties.fragments.addToBackStack
 
 object FragmentChanger {
     fun change(from: Fragment, to: Fragment, view: Int = R.id.mainActivityfragment) {
         from.activity?.supportFragmentManager
             ?.beginTransaction()
             ?.replace(view, to)
+            ?.addToBackStack(null)
             ?.commit()
     }
 }
